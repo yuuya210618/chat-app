fd60fa2878cf75a083b6007cc0bee9e07d837ba0
yuuya210618 <yuuya210618@gmail.com>
2023-07-20 23:51:53 +0900

    API実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,29 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, 
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+
 
          validates :name, presence: true
 
          has_many :room_users
          has_many :rooms, through: :room_users
          has_many :messages
+         has_many :sns_credentials
          
+         def self.from_omniauth(auth)
+          sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+          user = User.where(email: auth.info.email).first_or_initialize(
+     nickname: auth.info.name,
+       email: auth.info.email
+   )
+   if user.persisted?
+    sns.user = user
+    sns.save
+  end
+  { user: user, sns: sns }
+         end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_143034) do
+ActiveRecord::Schema.define(version: 2023_07_20_141803) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 2023_04_04_143034) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -76,4 +85,5 @@ ActiveRecord::Schema.define(version: 2023_04_04_143034) do
   add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
+  add_foreign_key "sns_credentials", "users"
 end
